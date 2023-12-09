d683182a7240af176c3db0490c3af39d96941053
Devansh Purohit <25509659+devd-99@users.noreply.github.com>
2023-12-09 15:46:43 -0500

    semseg mod

--- main_semseg.py
+++ main_semseg.py
@@ -63,168 +63,168 @@ val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True)
 unlabeled_loader = DataLoader(unlabeled_dataset,batch_size=16,shuffle=True)
 
 
-#training
-epochs=10
-shape_in = (11, 3, 128, 128)  # You need to adjust these dimensions based on your actual data
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
-logging.info(f"Using device: {device}")
+# #training
+# epochs=10
+# shape_in = (11, 3, 128, 128)  # You need to adjust these dimensions based on your actual data
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print(f"Using device: {device}")
+# logging.info(f"Using device: {device}")
 
-# Initialize the model
-model = SimVP(shape_in=shape_in).to(device)
-model.train()
+# # Initialize the model
+# model = SimVP(shape_in=shape_in).to(device)
+# model.train()
 
-frame_prediction_criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-# OneCycleLR Scheduler
-total_steps = epochs * (len(train_loader)+len(unlabeled_loader)) *2  # Total number of training steps
-scheduler = OneCycleLR(optimizer, max_lr=0.01, total_steps=total_steps)
+# frame_prediction_criterion = nn.MSELoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+# # OneCycleLR Scheduler
+# total_steps = epochs * (len(train_loader)+len(unlabeled_loader)) *2  # Total number of training steps
+# scheduler = OneCycleLR(optimizer, max_lr=0.01, total_steps=total_steps)
 
-print("before training")
-logging.info("This is an info message")
+# print("before training")
+# logging.info("This is an info message")
 
 
-# for epoch in range(int(configs['vp_epochs']):
-for epoch in range(epochs):
+# # for epoch in range(int(configs['vp_epochs']):
+# for epoch in range(epochs):
 
-    # first train on unlabeled dataset
-    for batch in unlabeled_loader:
-        images, _ = batch
+#     # first train on unlabeled dataset
+#     for batch in unlabeled_loader:
+#         images, _ = batch
         
-        input_frames = images[:, :11].to(device)
-        target_frame = images[:, 21].to(device)
+#         input_frames = images[:, :11].to(device)
+#         target_frame = images[:, 21].to(device)
 
-        # Forward pass
-        predicted_frames = model(input_frames)
-        predicted_target_frame = predicted_frames[:, -1]
+#         # Forward pass
+#         predicted_frames = model(input_frames)
+#         predicted_target_frame = predicted_frames[:, -1]
 
-        # Loss computation
-        loss = frame_prediction_criterion(predicted_target_frame, target_frame)
+#         # Loss computation
+#         loss = frame_prediction_criterion(predicted_target_frame, target_frame)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+#         # Backward and optimize
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
 
-        # Update the learning rate
-        scheduler.step()
+#         # Update the learning rate
+#         scheduler.step()
 
-        # print(f"Epoch [{epoch+1}/{epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {scheduler.get_last_lr()[0]}")
-        logging.info(f"Epoch [{epoch+1}/{epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {scheduler.get_last_lr()[0]}")
+#         # print(f"Epoch [{epoch+1}/{epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {scheduler.get_last_lr()[0]}")
+#         logging.info(f"Epoch [{epoch+1}/{epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {scheduler.get_last_lr()[0]}")
 
-for epoch in range(epochs):
-    # now train on training dataset
-    for batch in train_loader:
-        images, _ = batch
-        input_frames = images[:, :11].to(device)
-        target_frame = images[:, 21].to(device)
+# for epoch in range(epochs):
+#     # now train on training dataset
+#     for batch in train_loader:
+#         images, _ = batch
+#         input_frames = images[:, :11].to(device)
+#         target_frame = images[:, 21].to(device)
 
-        # Forward pass
-        predicted_frames = model(input_frames)
-        predicted_target_frame = predicted_frames[:, -1]
+#         # Forward pass
+#         predicted_frames = model(input_frames)
+#         predicted_target_frame = predicted_frames[:, -1]
 
-        # Loss computation
-        loss = frame_prediction_criterion(predicted_target_frame, target_frame)
+#         # Loss computation
+#         loss = frame_prediction_criterion(predicted_target_frame, target_frame)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+#         # Backward and optimize
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
 
-        # Update the learning rate
-        scheduler.step()
+#         # Update the learning rate
+#         scheduler.step()
 
-        # print(f"Epoch [{epoch+1}/{epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {scheduler.get_last_lr()[0]}")
-        logging.info(f"Epoch [{epoch+1}/{epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {scheduler.get_last_lr()[0]}")
+#         # print(f"Epoch [{epoch+1}/{epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {scheduler.get_last_lr()[0]}")
+#         logging.info(f"Epoch [{epoch+1}/{epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {scheduler.get_last_lr()[0]}")
 
-model_save_path = '../outs/models/my_model_' +str(datetime_formatted())+'.pth'
+# model_save_path = '../outs/models/my_model_' +str(datetime_formatted())+'.pth'
 
-# Save the model's state dictionary
-torch.save(model.state_dict(), model_save_path)
+# # Save the model's state dictionary
+# torch.save(model.state_dict(), model_save_path)
 
-# Inform the user
-print(f'Model saved to {model_save_path}')
-logging.info(f'Model saved to {model_save_path}')
-
-
-# model_save_path = 'my_model.pth'
-#files.download(model_save_path)
-shape_in = (11, 3, 128, 128)
-print(f"Using device: {device}")
-model = SimVP(shape_in=shape_in).to(device)
-
-# Load the state dictionary
-state_dict = torch.load(model_save_path)
-
-# Load the state dict into the model
-model.load_state_dict(state_dict)
+# # Inform the user
+# print(f'Model saved to {model_save_path}')
+# logging.info(f'Model saved to {model_save_path}')
 
 
-model.eval()
-model.to(device)
-mse_loss = nn.MSELoss()
-total_loss = 0.0
-with torch.no_grad():  # Disable gradient computation
-    for batch in val_loader:
-        images, _ = batch
-        input_frames = images[:, :11].to(device)  # First 11 frames
-        actual_22nd_frame = images[:, 21].to(device)
-        # Forward pass to get the predictions
-        predicted_frames = model(input_frames)
-        predicted_22nd_frame = predicted_frames[:, -1]
-        loss = mse_loss(predicted_22nd_frame, actual_22nd_frame)
-        total_loss += loss.item()
+# # model_save_path = 'my_model.pth'
+# #files.download(model_save_path)
+# shape_in = (11, 3, 128, 128)
+# print(f"Using device: {device}")
+# model = SimVP(shape_in=shape_in).to(device)
 
-# Calculate the average loss
-average_loss = total_loss / len(val_loader)
-print(f"Average MSE Loss on the validation dataset: {average_loss}")
-logging.info(f"Average MSE Loss on the validation dataset: {average_loss}")
+# # Load the state dictionary
+# state_dict = torch.load(model_save_path)
+
+# # Load the state dict into the model
+# model.load_state_dict(state_dict)
 
 
-batch = next(iter(val_loader))
-input_frames, _ = batch
-input_frames = input_frames.to(device)
+# model.eval()
+# model.to(device)
+# mse_loss = nn.MSELoss()
+# total_loss = 0.0
+# with torch.no_grad():  # Disable gradient computation
+#     for batch in val_loader:
+#         images, _ = batch
+#         input_frames = images[:, :11].to(device)  # First 11 frames
+#         actual_22nd_frame = images[:, 21].to(device)
+#         # Forward pass to get the predictions
+#         predicted_frames = model(input_frames)
+#         predicted_22nd_frame = predicted_frames[:, -1]
+#         loss = mse_loss(predicted_22nd_frame, actual_22nd_frame)
+#         total_loss += loss.item()
 
-# Predict the 22nd frame
-model.eval()
-with torch.no_grad():
-    predicted_frames = model(input_frames[:, :11])  # Use first 11 frames as input
-    predicted_22nd_frame = predicted_frames[:, -1]  # Extract the 22nd frame prediction
-
-# Move tensors to CPU for plotting
-predicted_22nd_frame = predicted_22nd_frame.cpu()
-actual_22nd_frame = input_frames[:, 21].cpu()  # Actual 22nd frame
-
-# Function to convert tensor to image
-def tensor_to_image(tensor):
-    tensor = tensor.squeeze(0)  # Remove batch dimension
-    tensor = tensor.permute(1, 2, 0)  # Change dimensions from CxHxW to HxWxC
-    tensor = tensor.numpy()
-    tensor = (tensor - tensor.min()) / (tensor.max() - tensor.min())  # Normalize to [0, 1]
-    return tensor
-
-# Convert tensors to images
-predicted_image = tensor_to_image(predicted_22nd_frame[0])  # First sample in the batch
-actual_image = tensor_to_image(actual_22nd_frame[0])  # First sample in the batch
-
-# Plot the images for comparison
-plt.figure(figsize=(12, 6))
-plt.subplot(1, 2, 1)
-plt.imshow(predicted_image)
-plt.title('Predicted 22nd Frame')
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
-plt.imshow(actual_image)
-plt.title('Actual 22nd Frame')
-plt.axis('off')
-
-# plt.show()
-plt.savefig('../outs/images/diff_plot_'+datetime_formatted()+'.png') 
+# # Calculate the average loss
+# average_loss = total_loss / len(val_loader)
+# print(f"Average MSE Loss on the validation dataset: {average_loss}")
+# logging.info(f"Average MSE Loss on the validation dataset: {average_loss}")
 
 
+# batch = next(iter(val_loader))
+# input_frames, _ = batch
+# input_frames = input_frames.to(device)
+
+# # Predict the 22nd frame
+# model.eval()
+# with torch.no_grad():
+#     predicted_frames = model(input_frames[:, :11])  # Use first 11 frames as input
+#     predicted_22nd_frame = predicted_frames[:, -1]  # Extract the 22nd frame prediction
+
+# # Move tensors to CPU for plotting
+# predicted_22nd_frame = predicted_22nd_frame.cpu()
+# actual_22nd_frame = input_frames[:, 21].cpu()  # Actual 22nd frame
+
+# # Function to convert tensor to image
+# def tensor_to_image(tensor):
+#     tensor = tensor.squeeze(0)  # Remove batch dimension
+#     tensor = tensor.permute(1, 2, 0)  # Change dimensions from CxHxW to HxWxC
+#     tensor = tensor.numpy()
+#     tensor = (tensor - tensor.min()) / (tensor.max() - tensor.min())  # Normalize to [0, 1]
+#     return tensor
+
+# # Convert tensors to images
+# predicted_image = tensor_to_image(predicted_22nd_frame[0])  # First sample in the batch
+# actual_image = tensor_to_image(actual_22nd_frame[0])  # First sample in the batch
+
+# # Plot the images for comparison
+# plt.figure(figsize=(12, 6))
+# plt.subplot(1, 2, 1)
+# plt.imshow(predicted_image)
+# plt.title('Predicted 22nd Frame')
+# plt.axis('off')
+
+# plt.subplot(1, 2, 2)
+# plt.imshow(actual_image)
+# plt.title('Actual 22nd Frame')
+# plt.axis('off')
+
+# # plt.show()
+# plt.savefig('../outs/images/diff_plot_'+datetime_formatted()+'.png') 
 
 
+
+#keep commented
 # unet training
 
 
@@ -294,7 +294,7 @@ modelunet2 = UNet(n_channels, n_classes).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(modelunet2.parameters(), lr=0.001)
 # Learning rate scheduler
-scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
+# scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
 
 # Number of epochs
 num_epochs = 200
@@ -324,10 +324,10 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         # Update the learning rate
-        scheduler.step(loss)
+        # scheduler.step(loss)
         running_loss += loss.item()
         # print(f"Epoch [{epoch+1}/{num_epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {optimizer.param_groups[0]['lr']}")
-        logging.info(f"Epoch [{epoch+1}/{num_epochs}], Step [{scheduler.last_epoch}/{total_steps}], Loss: {loss.item()}, LR: {optimizer.param_groups[0]['lr']}")
+        logging.info(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}, LR: {optimizer.param_groups[0]['lr']}")
     # Print training statistics
     train_loss = running_loss / len(train_loader)
     print(f'Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss}')
